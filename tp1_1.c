d5df4ccb21856be59cffdad232c57be902e1bebf
Benjafag <benja_fag_04@hotmail.com>
2025-03-26 01:29:24 -0300

    ejercicio 3.f

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+int numero = 15;
+int *puntero = &numero;
+
 int main()
 {
-  puts("Hola mundo");
+  printf("Hola mundo\n");
+  printf("Contenido del puntero: %d\n", *puntero);
+  printf("Direccion de memoria almacenada por el puntero: %p\n", puntero);
+  printf("Direccion de memoria de la variable: %p\n", &numero);
+  printf("Direccion de memoria del puntero: %p\n", &puntero);
+  printf("Memoria usada por la variable \"numero\": %zu bytes\n", sizeof(numero));
 }
